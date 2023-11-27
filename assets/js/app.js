3a05c6225d6b533942e0c78f872805465c716b8f
Mario Mata <mmatas1300@gmail.com>
2023-11-27 15:15:47 -0600

    Correcion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const nameRef = document.querySelector('.name');//se añadio .name en lugar de name | Se cambio de nombre por algo mas explicito 
+const blogRef = document.querySelector('.blog');//blog es una clase, se cambia #blog por .blog | Se cambio de nombre por algo mas explicito 
+const locationRef = document.querySelector('.location'); //| Se cambio de nombre por algo mas explicito 
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
-}
+async function displayUser(username){//La función se hace async debido a la promesa fetch
+  nameRef.textContent = 'Cargando...';
+ try{//Se aañdio try catch para consumir
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json();
+    nameRef.textContent = data.name;
+    blogRef.textContent = data.blog;
+    locationRef.textContent = data.location;//Se eliminaron comillas y el fotmato ${}
+  } catch(error){
+    handleError(error);
+  }
+
+ }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  nameRef.textContent = "Error: "+err;
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski');
